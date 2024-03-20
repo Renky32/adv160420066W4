7d965e95c9ad442176ba858d96026b55a83eb9e4
Renky32 <s160420066@student.ubaya.ac.id>
2024-03-20 20:01:07 +0700

    init

---
+++ app/src/main/java/com/example/adv1606420066w4/model/Model.kt
@@ -0,0 +1,13 @@
+package com.example.adv1606420066w4.model
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.adv1606420066w4.databinding.StudentListItemBinding
+
+data class Student(
+    val id : String?,
+    val name : String?,
+    val dob : String?,
+    val phone : String?,
+    val photoURL : String?,
+)
+
