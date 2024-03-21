e50b8889a15b561e4e3f8fcad58b488a66e25669
Renky32 <s160420066@student.ubaya.ac.id>
2024-03-21 13:09:49 +0700

    init

--- app/src/main/java/com/example/adv1606420066w4/view/StudentDetailFragment.kt
+++ app/src/main/java/com/example/adv1606420066w4/view/StudentDetailFragment.kt
@@ -5,7 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.adv1606420066w4.R
+import com.example.adv1606420066w4.databinding.FragmentStudentDetailBinding
+import com.example.adv1606420066w4.databinding.FragmentStudentListBinding
+import com.example.adv1606420066w4.viewmodel.DetailViewModel
+import com.example.adv1606420066w4.viewmodel.ListViewModel
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,13 +25,30 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class StudentDetailFragment : Fragment() {
+    private lateinit var viewModel: DetailViewModel
+    private lateinit var binding : FragmentStudentDetailBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_student_detail, container, false)
+        binding = FragmentStudentDetailBinding.inflate(inflater,container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this).get(DetailViewModel::class.java)
+        viewModel.fetch()
+
+        viewModel.studentLD.observe(viewLifecycleOwner, Observer {
+            binding.txtID.setText(it.id)
+            binding.txtName.setText(it.name)
+            binding.txtBOD.setText(it.dob)
+            binding.txtNumber.setText(it.phone)
+        })
     }
 
 }
